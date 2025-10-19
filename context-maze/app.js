7d826348f2bd14c733883c81c5258bf25e0b29ea
Daria Poberezhna <shytka90@gmail.com>
2025-10-20 01:38:25 +0200

    Context Maze functionality

--- context-maze/app.js
+++ context-maze/app.js
@@ -1,3 +1,52 @@
 'use strict';
 
-console.log('hello Daria');
+class ContextMaze {
+    constructor(name) {
+        this.name = name;
+        // this.log = this.log.bind(this);
+        // this.delayedLog = this.delayedLog.bind(this);
+    }
+
+    log() {
+        console.log(this.name);
+    }
+
+    delayedLog() {        
+        //1
+        const self = this;
+
+        setTimeout(function() {
+            console.log('1', self.name);
+        }
+        ,1000);
+
+        //2
+        setTimeout(() => {
+            console.log('2', this.name);
+        }
+        ,1000);
+
+        //3
+        setTimeout(function() {
+            console.log('3', this.name);
+        }.bind(this),1000);
+    }
+
+    nested(fn) {
+       return fn();
+    }
+
+    static createChained() {
+        const a = new ContextMaze("A");
+        const b = new ContextMaze("B");
+
+        a.log();               // A
+        b.log();               // B
+        a.nested(b.log.bind(b));       // B
+        b.nested(a.delayedLog.bind(a)); // A (3 варіанти через 1 сек)
+        const lost = a.log;
+        lost.call(b);   // B   
+    }
+}  
+
+ContextMaze.createChained();
